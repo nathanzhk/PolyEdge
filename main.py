d23836a3d5a38b6f80e24e1ccba2f9feef33f6bb
Nathan <nathanzhk@gmail.com>
2026-02-12 22:33:24 -0800

    feat: add Polymarket market client models

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from domain.market import Btc5mMarket
+
+
 def main():
-    print("Hello!")
+    market = Btc5mMarket.now()
+    print(market)
+    next_market = Btc5mMarket.next()
+    print(next_market)
 
 
 if __name__ == "__main__":
